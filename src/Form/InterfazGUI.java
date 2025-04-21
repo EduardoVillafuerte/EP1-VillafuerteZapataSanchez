fdca7759af04724a6be4c26d59fcbe639724798d
Yusely20 <zapatayuse.20.04@gmail.com>
2025-04-21 14:15:34 -0500

    Interfaz1

---
+++ src/Form/InterfazGUI.java
@@ -0,0 +1,13 @@
+package Form;
+
+import javax.swing.*;
+
+public class InterfazGUI {
+    private JPanel pGeneral;
+    private JTextArea textArea1;
+    private JButton button1;
+    private JButton button2;
+    private JTextField textField1;
+    private JButton button3;
+    private JButton button4;
+}
