17f577fa8eb31fe7f0ecfb8c14bcda6af7d4b12c
EduardoVillafuerte <148821432+EduardoVillafuerte@users.noreply.github.com>
2025-04-21 14:27:55 -0500

    Clases

--- src/Clases/ColaDigimon.java
+++ src/Clases/ColaDigimon.java
@@ -1,4 +1,22 @@
 package Clases;
 
+import javax.swing.*;
+import java.util.LinkedList;
+import java.util.Queue;
+
 public class ColaDigimon {
+    private Queue<Digimon> cola = new LinkedList<>();
+
+    public ColaDigimon(Queue<Digimon> cola) {
+        this.cola.add(new Digimon());
+        this.cola.add(new Digimon());
+        this.cola.add(new Digimon());
+        this.cola.add(new Digimon());
+        this.cola.add(new Digimon());
+    }
+
+    public void actualizarTabla(JTextArea txtArea){
+        //txtArea.append();
+    }
+
 }
